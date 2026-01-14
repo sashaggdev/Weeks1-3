c5b6923f417b85eb81737d0a31c8d474e955ac8e
sashaggdev <arcanasasha@gmail.com>
2026-01-14 10:14:47 -0500

    Pulse script

---
+++ Assets/pulse.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class pulse : MonoBehaviour
+{
+    public AnimationCurve curve;
+    public float t = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        if (t > 1)
+        {
+            t = 0;
+        }
+
+        float y = curve.Evaluate(t);
+
+        transform.localScale = Vector3.one * curve.Evaluate(t);
+    }
+}
